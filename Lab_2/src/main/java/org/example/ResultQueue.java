e23fce66890330d0bd311ef62f959a94353a1da4
Nick Pozniak <87877391+imwrdo@users.noreply.github.com>
2024-03-15 23:01:51 +0100

    Add files via upload

---
+++ Lab_2/src/main/java/org/example/ResultQueue.java
@@ -0,0 +1,59 @@
+package org.example;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class QueueOut{
+    private final Integer ID;
+    private final Double piValue;
+    private final Double percents;
+    public QueueOut(Integer ID, Double piValue, Double percents){
+        this.ID=ID;
+        this.piValue=piValue;
+        this.percents=percents;
+    }
+    public int getID(){
+        return ID;
+    }
+
+    public double getPiValue(){
+        return piValue;
+    }
+    public double getPercents(){
+        return percents;
+    }
+    @Override
+    public String toString() {
+        return "\n" +
+                "ID= " + ID +
+                ", pi= " + piValue +
+                ", percents= " + percents+
+                ' ';
+    }
+}
+public class ResultQueue {
+    LinkedList<QueueOut> resultQueue = new LinkedList<>();
+
+    public synchronized void addResult(int ID,double piValue, double percents){
+        try {
+            resultQueue.addLast(new QueueOut(ID, piValue,percents));
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid result: " + ID);
+            // You might choose to log the error, throw a custom exception, or handle it differently based on your requirements
+        }
+    }
+    public synchronized void removeResult(int taskId) {
+        // Find and remove element by ID
+        for (QueueOut result : resultQueue) {
+            if (result.getID() == taskId) {
+                resultQueue.remove(result);
+                break; // Exit loop once element is removed
+            }
+        }
+    }
+    @Override
+    public String toString() {
+        return "" + resultQueue;
+    }
+
+
+}
---
+++ Lab_2/src/main/java/org/example/TaskQueue.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class TaskQueue {
+    private final Queue<String> taskQueue = new LinkedList<>();
+
+    public synchronized void AddTask(int taskID, int taskNumber) {
+        this.taskQueue.add(taskID + " " + taskNumber);
+        notify();
+    }
+
+    public synchronized String GetTask() throws InterruptedException {
+        if (taskQueue.isEmpty()) {
+            wait();
+        }
+        return taskQueue.poll();
+    }
+}
+
