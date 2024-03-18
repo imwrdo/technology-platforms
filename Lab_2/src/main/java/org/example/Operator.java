fe1cb2ad0291a4f099dec465ec1646a72cadd7e0
Nick Pozniak <87877391+imwrdo@users.noreply.github.com>
2024-03-18 23:56:08 +0100

    Add files via upload

---
+++ Lab_2/src/main/java/org/example/Operator.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.lang.Math;
+
+public class Operator implements Runnable{
+    public final TaskQueue taskQueue;
+    public final ResultQueue resultQueue;
+
+    public Operator(TaskQueue taskQueue, ResultQueue resultQueue) {
+        this.taskQueue = taskQueue;
+        this.resultQueue = resultQueue;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                String[] task = taskQueue.GetTask().split(" ");
+                int taskId = Integer.parseInt(task[0]);
+                int taskNumber = Integer.parseInt(task[1]);
+                float res = 0;
+                for (int i =1;i<taskNumber;i++ ){
+                    res +=(Math.pow(-1,i-1)/(2*i-1));
+                    resultQueue.addResult(taskId,res*4,(float)i/(taskNumber*100));
+                    Thread.sleep(5);
+                    resultQueue.removeResult(taskId);
+                }
+                System.out.print("Pi value = " + res*4 + ". Percents: " + 100.0 + "%.\n");
+                resultQueue.addResult(taskId, res * 4, 100.0);
+
+            }catch (InterruptedException e){
+                break;
+            }
+        }
+    }
+}
